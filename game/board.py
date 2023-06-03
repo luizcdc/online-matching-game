87fb3ba9f785a691b85d37e60bf52d4d4151bb8c
Luiz Cavalcanti <>
2023-06-03 17:17:35 -0300

    Cria ServerBoard e ServerCard e altera Board e Card

--- game/board.py
+++ game/board.py
@@ -2,20 +2,21 @@
 import pygame
 from random import shuffle
 from game import constants
+
 pygame.init()
 fonte_numeros = pygame.font.SysFont("comicsans", 42)
 
-class Board:
-    def __init__(self, position: tuple = constants.BOARD_POS):
-        self.cards = []
+
+class ServerBoard:
+
+    def __init__(self):
+        self.cards: list[list[ServerCard]] = []
         self.acertos = []
         for i in range(constants.NUM_LINHAS):
             self.cards.append([])
             for j in range(constants.NUM_LINHAS):
-                rect = pygame.Rect(100 + position[0] + j * (Card.size[0] + 10), 25 + position[1] + i * (Card.size[1] + 10), *Card.size)
-                self.cards[i].append(Card(rect))
+                self.cards[i].append(ServerCard())
         self.embaralhar_cartas()
-        self.posicao = position
 
     def embaralhar_cartas(self):
         options = [i for i in range(1, constants.NUM_LINHAS ** 2 // 2 + 1)]
@@ -24,18 +25,6 @@ class Board:
         for i in range(constants.NUM_LINHAS):
             for j in range(constants.NUM_LINHAS):
                 self.cards[i][j].numero = options.pop()
-
-    def draw(self, janela, escolhas):
-        for i in range(constants.NUM_LINHAS):
-            for j in range(constants.NUM_LINHAS):
-                self.cards[i][j].draw(janela, self.cards[i][j] in escolhas)
-
-    def click(self, mouse_pos, janela):
-        for i in range(constants.NUM_LINHAS):
-            for j in range(constants.NUM_LINHAS):
-                if self.cards[i][j].rect.collidepoint(mouse_pos):
-                    self.cards[i][j].draw(janela, True)
-                    return self.cards[i][j]
 
     def check(self, escolhas):
         if escolhas[0].numero == escolhas[1].numero and escolhas[0] not in self.acertos:
@@ -46,6 +35,23 @@ class Board:
                         self.acertos.append(self.cards[i][j])
             return True
         return False
+
+    def get_card_by_pos(self, i, j):
+        if self.coord_is_valida((i, j)):
+            return self.cards[i][j]
+        else:
+            return None
+
+    @staticmethod
+    def coord_is_valida(coord):
+        return 0 <= coord[0] < constants.NUM_LINHAS and 0 <= coord[1] < constants.NUM_LINHAS
+
+
+class ServerCard:
+
+    def __init__(self, numero: int = 0):
+        self.numero = numero
+        self.virada = False
 
 
 class Card:
@@ -72,3 +78,40 @@ class Card:
         numero_imprimir = fonte_numeros.render(str(self.numero), True, color)
         janela.blit(numero_imprimir, (self.rect.centerx - numero_imprimir.get_width() // 2,
                                       self.rect.centery - numero_imprimir.get_height() // 2))
+
+
+class Board:
+    def __init__(self, position: tuple = constants.BOARD_POS):
+        self.cards = []
+        self.acertos = []
+        for i in range(constants.NUM_LINHAS):
+            self.cards.append([])
+            for j in range(constants.NUM_LINHAS):
+                rect = pygame.Rect(100 + position[0] + j * (Card.size[0] + 10),
+                                   25 + position[1] + i * (Card.size[1] + 10), *Card.size)
+                self.cards[i].append(Card(rect))
+
+        self.posicao = position
+
+    def draw(self, janela, escolhas):
+        for i in range(constants.NUM_LINHAS):
+            for j in range(constants.NUM_LINHAS):
+                if self.cards[i][j] in escolhas:
+                    self.cards[i][j].draw(janela, True)
+
+    def click(self, mouse_pos, janela):
+        for i in range(constants.NUM_LINHAS):
+            for j in range(constants.NUM_LINHAS):
+                if self.cards[i][j].rect.collidepoint(mouse_pos):
+                    self.cards[i][j].draw(janela, True)
+                    return self.cards[i][j], i, j
+
+    def check(self, escolhas):
+        if escolhas[0].numero == escolhas[1].numero and escolhas[0] not in self.acertos:
+            for i in range(constants.NUM_LINHAS):
+                for j in range(constants.NUM_LINHAS):
+                    if self.cards[i][j] in escolhas:
+                        self.cards[i][j].virada = True
+                        self.acertos.append(self.cards[i][j])
+            return True
+        return False
