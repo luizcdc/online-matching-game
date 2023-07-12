73bc3d6d43c193c3ac5447ea328e569c4da38658
Luiz Cavalcanti <>
2023-07-12 17:27:27 -0300

    Adiciona docstrings em game.py

--- game/game.py
+++ game/game.py
@@ -34,10 +34,12 @@ class Game:
         self.state = None
 
     def add_player(self, player_name: str):
+        """Registra o nome de um jogador localmente"""
         assert len(self.player_names) < 2, "Já existem dois jogadores"
         self.player_names.append(player_name)
 
     def start_game(self, *, eu_comeco: Optional[bool] = None):
+        """Inicia o jogo, marcando de quem é a jogada inicial"""
         assert len(self.player_names) == 2, "Não há dois jogadores"
         assert self.is_client or eu_comeco is None
         if self.is_client:
@@ -45,11 +47,13 @@ class Game:
         self.resetar_jogada()
 
     def resetar_jogada(self):
+        """Descarta escolhas e muda o estado do jogo para o estado inicial da jogada"""
         self.escolhas = []
         if self.is_client:
             self.state = GameState.ESCOLHENDO_1 if self.minha_vez else GameState.OPONENTE_ESCOLHENDO_1
 
     def iniciar_vez_oponente(self):
+        """Muda o estado e reinicia a jogada para o oponente"""
         assert self.is_client
         assert self.minha_vez
         self.minha_vez = False
@@ -57,6 +61,7 @@ class Game:
         self.resetar_jogada()
 
     def iniciar_minha_vez(self):
+        """Muda o estado e reinicia a jogada para o jogador local"""
         assert self.is_client
         assert self.minha_vez is False
         self.minha_vez = True
@@ -73,7 +78,9 @@ class Game:
         self.resetar_jogada()
 
     def escolha_1_foi_feita(self):
+        """Verifica se a primeira escolha foi feita"""
         return self.escolhas and len(self.escolhas) >= 1
 
     def escolha_2_foi_feita(self):
+        """Verifica se a segunda escolha foi feita"""
         return self.escolhas and len(self.escolhas) == 2
